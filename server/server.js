198d09cb4dfcab48048d54291b24cb6e47be13b1
Dev Prasad <devpatra2000@gmail.com>
2025-04-26 03:16:58 +0530

    CORS Changed

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://ai-safety-incidents.vercel.app', // Your Vercel deployment URL
+    'https://incident-management-system-d5f6.onrender.com'
   ],
   methods: ['GET', 'POST', 'DELETE'],
   credentials: true,
