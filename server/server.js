b6aa9796338e5e002b4cad3be845009c921979a6
Dev Prasad <devpatra2000@gmail.com>
2025-04-26 03:25:46 +0530

    Server Cors Fixed

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://incident-management-system-d5f6.onrender.com'
+    'https://incident-management-system-mauve.vercel.app/'
   ],
   methods: ['GET', 'POST', 'DELETE'],
   credentials: true,
